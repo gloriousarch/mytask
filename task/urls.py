7d2f185fcd854458390ce09aa58f8710f25092f6
gloriousarch <2637107b@student.gla.ac.uk>
2022-03-12 23:42:15 +0000

    Implement login and register endpoints with an additional authentication test endpoint.
    - Still needs further testing
    - No templates yet so no way to register or login for now

--- task/urls.py
+++ task/urls.py
@@ -7,5 +7,8 @@ app_name = 'task'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-
+    path('register/', views.user_register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('login-test/', views.test_login, name='login-test'),
+    path('logout/', views.user_logout, name='logout')
 ]
--- task/views.py
+++ task/views.py
@@ -1,10 +1,70 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import reverse, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from task.forms import UserForm
+from task.models import UserProfile
+
+
 def index(request):
     return HttpResponse("This is home page ")
+
 
 def about(request):
     return HttpResponse("This is about page ")
 
+
+def user_register(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+
+        if user_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = UserProfile(user=user)
+            profile.save()
+
+            registered = True
+        else:
+            return HttpResponse(str(user_form.errors))
+    else:
+        user_form = UserForm()
+
+    return HttpResponse('Register Page ¯\\_(ツ)_/¯ ')
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('tasks:index'))
+            else:
+                return HttpResponse('Account is disabled :(')
+        else:
+            return HttpResponse('Invalid username/password pair.')
+    else:
+        return HttpResponse('Login Page 👍')
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('task:index'))
+
+
+def test_login(request):
+    if request.user.is_authenticated:
+        return HttpResponse('Congrats, you are logged in!')
+    else:
+        return HttpResponse('Gotta login bruv')
