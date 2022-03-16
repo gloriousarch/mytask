abfd423b7308461456a2cfacb3eb04f6703d260d
Yi Zhou <2576565z@student.gla.ac.uk>
2022-03-17 05:38:47 +0800

    urls

--- task/urls.py
+++ task/urls.py
@@ -10,7 +10,13 @@ urlpatterns = [
     path('admin/', views.admin, name='admin'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
-    
+    path('taskpage/', views.taskpage, name='taskpage'),
+    path('taskpage/taskpageid/', views.taskpageid, name='taskpageid'),
+    path('usercenter/', views.usercenter, name='usercenter'),
+    path('usercenter/posttask/', views.posttask, name='posttask'),
+    path('usercenter/accepttask/', views.accepttask, name='accepttask'),
+    path('usercenter/modifytheinformation/', views.modifytheinformation, name='modifytheinformation'),
+    path('usercenter/changepassword/', views.changepassword, name='changepassword'),
 
     # path('login/', views.user_login, name='login'),
     # path('login-test/', views.test_login, name='login-test'),
