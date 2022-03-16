c4dfa947855e68117edf31ccdfe825df54979be0
MaxWang8 <2561238W@student.gla.ac.uk>
2022-03-16 21:30:51 +0000

    url start for zhouyi

--- task/urls.py
+++ task/urls.py
@@ -7,8 +7,12 @@ app_name = 'task'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('register/', views.user_register, name='register'),
-    path('login/', views.user_login, name='login'),
-    path('login-test/', views.test_login, name='login-test'),
-    path('logout/', views.user_logout, name='logout')
+    path('admin/', views.admin, name='admin'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    
+
+    # path('login/', views.user_login, name='login'),
+    # path('login-test/', views.test_login, name='login-test'),
+    # path('logout/', views.user_logout, name='logout')
 ]
